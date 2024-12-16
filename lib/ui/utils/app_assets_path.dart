c51667ac865bd5901d7d51342ae6285decdeb8b0
ShougotMollik <shougotmollik@gmail.com>
2024-12-16 17:14:48 +0600

    - Structure File utils - Design Login Screen

---
+++ lib/ui/utils/app_assets_path.dart
@@ -0,0 +1,7 @@
+class AppAssetsPath {
+  static const String logo = 'assets/logo/logo.png';
+  static const String onboardingImg =
+      'assets/images/Blinkit Onboarding Screen.png';
+  static const String logo2 = 'assets/logo/logo2.png';
+  static const String logo3 = 'assets/logo/logo3.png';
+}
